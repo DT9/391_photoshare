28bff6305fb4c16fe0273a214854cc66df0321c6
denweny <dtruong1@ualberta.ca>
2016-03-20 17:27:30 -0600

    upload html

---
+++ header.php
@@ -0,0 +1,26 @@
+
+<nav class="navbar navbar-default navbar-fixed-top">
+	<div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a href="#" title="Home">
+                <img class="img-responsive" src="images/logo.png" style="height:30px; margin-top:10px;" alt="Photoshare" />
+            </a>
+		</div>
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav navbar-right">
+				<li class="info"><a href="#about" class="smooth-scroll">About</a></li>
+				<li class="danger"><a href="#projects" class="smooth-scroll">Projects</a></li>
+				<li class="warning"><a href="#team" class="smooth-scroll">Team</a></li>
+				<li class="primary"><a href="#contact" class="smooth-scroll">Contact</a></li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div><!-- /.container-fluid -->
+</nav>
