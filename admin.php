e4743cf0f0eadf58c63f6644881605411942bf7d
denweny <dtruong1@ualberta.ca>
2016-03-21 14:44:51 -0600

    admin template up

--- admin.php
+++ admin.php
@@ -29,11 +29,7 @@
     <![endif]-->
 
 </head>
-<?php
-//generate morris chart
 
-
-?>
 <body>
 
     <div id="wrapper">
@@ -474,9 +470,79 @@
 
     </div>
     <!-- /#wrapper -->
-
     <!-- jQuery -->
-
 </body>
+<?php
+//DB connect
+//Display # image for ea user,subj and/or period/time(wk,mth,yr)
+//display the number of images for each user; or display the number of images for each subject for each month.
+
+    The user must be able to perform generalization (roll up) and specialization (drill down)  on three levels of time hierarchies,  i.e., weekly, monthly, and yearly.
+
+    To speed up the OLAP operations, one might materialize the data cube information by creating a fact table/view storing all  information needed, including the lowest level of time hierarchy.
+
+?>
+
+<script type="text/javascript">
+    Morris.Area({
+        element: 'morris-area-chart',
+        data: [{
+            period: '2010 Q1',
+            iphone: 2666,
+            ipad: null,
+            itouch: 2647
+        }, {
+            period: '2010 Q2',
+            iphone: 2778,
+            ipad: 2294,
+            itouch: 2441
+        }, {
+            period: '2010 Q3',
+            iphone: 4912,
+            ipad: 1969,
+            itouch: 2501
+        }, {
+            period: '2010 Q4',
+            iphone: 3767,
+            ipad: 3597,
+            itouch: 5689
+        }, {
+            period: '2011 Q1',
+            iphone: 6810,
+            ipad: 1914,
+            itouch: 2293
+        }, {
+            period: '2011 Q2',
+            iphone: 5670,
+            ipad: 4293,
+            itouch: 1881
+        }, {
+            period: '2011 Q3',
+            iphone: 4820,
+            ipad: 3795,
+            itouch: 1588
+        }, {
+            period: '2011 Q4',
+            iphone: 15073,
+            ipad: 5967,
+            itouch: 5175
+        }, {
+            period: '2012 Q1',
+            iphone: 10687,
+            ipad: 4460,
+            itouch: 2028
+        }, {
+            period: '2012 Q2',
+            iphone: 8432,
+            ipad: 5713,
+            itouch: 1791
+        }],
+        xkey: 'period',
+        ykeys: ['iphone', 'ipad', 'itouch'],
+        labels: ['iPhone', 'iPad', 'iPod Touch'],
+        pointSize: 2,
+        hideHover: 'auto',
+        resize: true
+    });</script>
 
 </html>
