1f7e59b83b1dc4e6b43d29ce8aa5cfb0008b518b
Jian Le Tang <jianle@cs.ualberta.ca>
2016-03-23 16:53:22 -0600

    gg

--- index.php
+++ index.php
@@ -9,18 +9,8 @@ include("connection_database.php");
             //get the input
             $user=$_POST['username'];
             $pswd=$_POST['password'];
-            $fn=$_POST['firstname'];
-            $ln=$_POST['lastname'];
-            $address=$_POST['address'];
-            $email=$_POST['email'];
-            $phone=$_POST['phonenumber'];
-            
-            
-				if ($user==''or $pswd=='' or $fn=='' or $ln=='' or $address=='' or $email=='' or $phone==''){
-				header("location:http://consort.cs.ualberta.ca/~jianle/database_site/391_photoshare/signup.html");	    
-	    		exit;				
-				}
-				
+            //if($user==''||$pswd==''){echo 'error';}
+			
 	    ini_set('display_errors', 1);
 	    error_reporting(E_ALL);
 	    
@@ -32,31 +22,32 @@ include("connection_database.php");
 	    }
  	///////////////////////////////////////////////////////////////////////////////////
  	//checking if username and password in system 
- 		 $sql='select count(*) from Users where user_name=\''.$user.'\' or password=\''.$pswd.'\'';
- 		 $sql.='insert into Users (user_name, password) values (\''.$user.'\',\''.$pswd.'\')';
- 		 $sql.='insert into persons (user_name, first_name, last_name, adress, email, phone) values (\''.$user.'\',\''.$pswd.'\',\''.$fn.'\',\''.$ln.'\',\''.$adress.'\',\''.$email.'\',\''.$phone.'\'');
+ 		 $sql='select count(*) from Users where user_name=\''.$user.'\' and password=\''.$pswd.'\'';
+ 		 $sql='select count(*) from Users where user_name=\''.$user.'\'';
+ 		          
 	    echo $sql;
 	    //Prepare sql using conn and returns the statement identifier
 	    $stid = oci_parse($conn, $sql);
 	    
 	    //Execute a statement returned from oci_parse()
 	    $res=oci_execute($stid);
-	    //$r='1';
-	    while ($row=oci_fetch_array($stid,OCI_BOTH)){$r= $row[0];}
+	    $r='1';
+	    while ($row=oci_fetch_array($stid,OCI_BOTH)){$r= $row[1];}
 	    //$row=oci_fetch_array($stid,OCI_BOTH)
-	    echo $r;
+	    //echo $r;
 	    oci_free_statement($stid);
 	    oci_close($conn);
-      
+	    echo "r is $r";
+      /*
 	    $result=FALSE;
-	    if ($r!='0'){ $result=TRUE;}
+	    if ($r=='0'){ $result=TRUE;}
 	    echo $result;
 	    if ($result){
-	    	header("location:http://consort.cs.ualberta.ca/~jianle/database_site/391_photoshare/signup.html");
+	    	header("location:http://consort.cs.ualberta.ca/~jianle/database_site/391_photoshare/new_index.html");
 		 	echo "error";	    
 	    	exit;
 	    }
-	    
+	    */
 	    echo "good good";
 	    
 	    
