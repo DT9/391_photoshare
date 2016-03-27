3f51a92a8c54fd6a87d0218baf1166c37392a489
denweny <dtruong1@ualberta.ca>
2016-03-27 16:57:40 -0600

    more pics

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 sqlplus -s /nolog <<EOF
 connect dtruong1/hunter23
 @setup.sql
+commit;
+quit
+EOF
+sqlplus -s /nolog <<EOF
+connect dtruong1/hunter23
 @ins.sql
 commit;
 quit
@@ -11,6 +16,12 @@ sqlldr control=lob.ctl userid=dtruong1/hunter23
 sqlplus -s /nolog <<EOF
 connect jianle/drag0ngamer
 @setup.sql
+commit;
+quit
+EOF
+
+sqlplus -s /nolog <<EOF
+connect jianle/drag0ngamer
 @ins.sql
 commit;
 quit
@@ -21,6 +32,12 @@ sqlldr control=lob.ctl userid=jianle/drag0ngamer
 sqlplus -s /nolog <<EOF
 connect chengyao/chengyao00308900
 @setup.sql
+commit;
+quit
+EOF
+
+sqlplus -s /nolog <<EOF
+connect chengyao/chengyao00308900
 @ins.sql
 commit;
 quit
--- setup.sql
+++ setup.sql
@@ -42,7 +42,6 @@ END;
 
 --Create sequences for auto-incr ids
 create sequence test_seq start with 1 increment by 1 nomaxvalue;
-create sequence picsbro start with 4 increment by 1 nomaxvalue;
 
 //creates tables
 CREATE TABLE users (
