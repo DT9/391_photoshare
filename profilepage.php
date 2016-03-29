476db1a144983dca576aa86ca0d6ea19c827d8f6
Jian Le Tang <jianle@cs.ualberta.ca>
2016-03-28 19:39:52 -0600

    john

--- profilepage.php
+++ profilepage.php
@@ -35,11 +35,6 @@
 	    oci_free_statement($stid);
 	    oci_close($conn);			
 	    /////////////////////end get user's info///////////////////////////////
-include("connection_database.php");
-session_start();
-	//connect();
-	//echo "<h1>hello</h1>";
-	$user=$_SESSION['user-name'];
 ?>
 
 <html lang="en">
@@ -74,24 +69,25 @@ session_start();
                         <li><a href="new_index.html">LOGOUT</a></li>
                     </ul>
                 </div>
-                <div id="search_form"><form action="/search.php" method="get"><input name="s" type="text" size="9" maxlength="30">
+                <!--<div id="search_form"><form action="search.php" method="get"><input name="s" type="text" size="9" maxlength="30">-->
                     </form>
                 </div>           
                 </div>
             
 
-
-					 <form action="search_page.php">
+<!----------------------- search form ------------------------------------- -->
+					 <form name="seach-form" method="post" action="search_page.php">
             	Search Gallery:
             	<p>
+            	<!-- search by date -->
             	<label for="from">From</label>
 					<input type="text" id="from" name="from">
 					<label for="to">to</label>
 					<input type="text" id="to" name="to">
 					</p>
             	
-
             	<p>
+            	<!-- search by subject -->
             	Enter Key Word:<input type ="search" id = "keysearch" name= "keysearch">
             	<input type="submit"></p>
             	
@@ -115,8 +111,6 @@ session_start();
                  </div>
                 
                 
-
-                <input class="text-input" id="user" type="text" value="<?php echo $_SESSION['user-name'];?>"/>
                     <div id="up_izq"><h3>GALLERY</h3></div>
                     
                     
