d15b8241cd1951cb1a7f3d159e976e670db2826c
Jian Le Tang <jianle@cs.ualberta.ca>
2016-03-29 17:05:54 -0600

    search stuff

--- search_page.php
+++ search_page.php
@@ -14,14 +14,14 @@ $keysearch = $_REQUEST['keysearch'];
 
 //get single word for keysearch
 echo $keysearch;
-$arr = explode(' ', trim($keysearch));
+$arr=str_replace(' ', '&', $keysearch);
+//$arr = explode(' ', trim($keysearch));
 //$arrfrom = explode(' ', trim($from));
-$num=count($arr);
-echo $num;
+
 
 echo "from date = $from";
 echo "to date = $to";
-echo "Keyword = $arr[2]";
+echo "Keyword = $arr ";
 
 
 
@@ -31,33 +31,56 @@ if (empty($arr[0])&&empty($from)&&empty($to)){
 	echo "empty";
 	//redirect back
 	}
+	
+	
 //else if date part empty
 elseif (!empty($arr[0])){
 	//all parts 
 	if(!empty($from)&&!empty($to)){
-	$sql='select photo_id from images where timing between \''.$from.'\' and \''.$to.'\'';	
-	echo "all parts";
+		$sql='select photo_id from images where CONTAINS(subject, \''.$arr.'\', 1)>0 and timing between \''.$from.'\' and \''.$to.'\'';	
+		echo "all parts";
 	}
-	elseif(empty($from)&&empty($to)){echo "only subject";}
-	//subject and to only
+	
+	//only subject
+	elseif(empty($from)&&empty($to)){
+		$sql='select photo_id from images where CONTAINS(subject, \''.$arr.'\', 1)>0';
+		echo "only subject";
+		}
+		
+	//subject and to only	
 	elseif (empty($from)){
-	echo "subject and to only";	
+		$sql='select photo_id from images where CONTAINS(subject, \''.$arr.'\', 1)>0 and timing between (select min(timing) from images) and \''.$to.'\'';
+		echo "subject and to only";
+			
 	}
 	//subject and from only
-	else{ echo "subject and from only";}
+	else{ 
+		$sql='select photo_id from images where CONTAINS(subject, \''.$arr.'\', 1)>0 and timing between \''.$from.'\' and sysdate';	
+		echo "subject and from only";}
 }
+
 //else if all date part no subject
 elseif (!empty($from)&&!empty($to)){
 	$sql='select photo_id from images where timing between \''.$from.'\' and \''.$to.'\'';	
-	echo $sql;
-	echo "all date no subject";}
+	$sql1='select count(photo_id) from images where timing between \''.$from.'\' and \''.$to.'\'';	
+	echo "all date no subject";
+	}
+	
 //else if from date entered only
-elseif (empty($to)){echo "date from only";}
+elseif (empty($to)){
+	
+	$sql=	'select photo_id from images where timing between \''.$from.'\' and sysdate';	
+	echo "date from only";
+	}
+	
 //else, if to date empty 
-else {echo "date to only";}
+else {
+	$sql= 'select photo_id from images where timing between (select min(timing) from images) and \''.$to.'\'';	
+	echo "date to only";}
 
 
-
+echo $sql;
+echo "number of pics: $sql1";
 
      $conn = connect();   
 
@@ -66,9 +89,23 @@ if (!$conn) {
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
 
-$sql= 
+	    $stid = oci_parse($conn, $sql);
+	    
+	    //Execute a statement returned from oci_parse()
+	    $res=oci_execute($stid);
+	    
+	    while ($row=oci_fetch_array($stid,OCI_BOTH)){
+	    	$r= $row['PHOTO_ID'];
+	    	//echo "r is $r";
+	    	echo '<a href="display.php?id='.$r.'"><img src="pullimage.php?id='.$r.'&type=thumbnail" width="175" height="200" /></a>';
+	    
+	    
+	    
+	    }
+	    
+	    
+	    oci_free_statement($stid);
+	    oci_close($conn);
 
-
-      oci_close($conn);
 
 ?>
