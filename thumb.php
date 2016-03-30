ca836de0d7cb7fe41f4e289af378ded7e4372690
r-hu1 <randyhu3089@gmail.com>
2016-03-30 17:22:29 -0600

    admin dies when not admin

--- thumb.php
+++ thumb.php
@@ -44,7 +44,6 @@
 	}
 	elseif ($option == "3") {
 		//top 5 popular images
-
 		$query = "select photo_id, count(photo_id) as visits from photo_count where ROWNUM <=5 group by photo_id order by visits desc";
 				//$query = "select photo_id from images where permitted = '1' or owner_name = '$user' or permitted in (select group_id from group_lists where friend_id = '$user' union select group_id from groups where user_name = '$user' )";
 	}
